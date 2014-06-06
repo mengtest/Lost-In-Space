14ba94b29560826dae0b45ce7ef789f712160ced
Nick Mudry <Nick@Nicks-Mac-mini.local>
2014-06-05 21:49:01 -0400

    Fixed Bug #7
    Quickly fixed the main menu bug, #7 and pushing the fix to the master
    branch.

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.loadedLevelName == "Main Menu")
+		{
+			Time.timeScale = 1f;
+		}
+	}
+}
