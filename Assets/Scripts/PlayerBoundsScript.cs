5327f8f743e2e6518a629a6e0c4fb5d7e866d3d8
Nick Mudry <Nick@Nicholass-MacBook-Air.local>
2014-06-06 13:37:34 -0400

    Temporary Fix for Bug #13
    You can still go off screen on taller iOS screens, such as iPhone 5.
    Needs a minor fix but other than that, it's all good!

---
+++ Assets/Scripts/PlayerBoundsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBoundsScript : MonoBehaviour {
+	public float bounds = 3.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.x < bounds * -1f)
+		{
+			gameObject.transform.position = new Vector2(bounds * -1f, gameObject.transform.position.y);
+		}
+		if (gameObject.transform.position.x > bounds)
+		{
+			gameObject.transform.position = new Vector2(bounds, gameObject.transform.position.y);
+		}
+
+	}
+}
