3640ebc11b649f1e42b15ef86e4010a28d47b575
Nick Mudry <Nick@Nicks-Mac-mini.local>
2014-06-11 22:48:47 -0400

    Revert "Gamepad Pauses Game"
    This reverts commit 93ad74f6dbb2ab1b7456e2e94daa76549af4ddc5.

--- Assets/Scripts/GamePadScript.cs
+++ Assets/Scripts/GamePadScript.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 public class GamePadScript : MonoBehaviour {
 	public float speedModifier = 1f; //Modifier for the speed. Multiplies, so anything less than 0 (ex: .5f) slows it down.
-	GameObject levelSettings;
-	bool pauseDown;
 	// Use this for initialization
 	void Start () {
-		levelSettings = GameObject.Find ("Level Settings");
-		pauseDown = false;
+	
 	}
 	
 	// Update is called once per frame
@@ -16,19 +13,7 @@ public class GamePadScript : MonoBehaviour {
 		if (Input.GetAxis("Horizontal") != 0)
 		{
 			gameObject.transform.position = new Vector3(gameObject.transform.position.x + Input.GetAxis ("Horizontal") * speedModifier, gameObject.transform.position.y);
+			Debug.Log ("Going Left! + " + Input.GetAxis("Horizontal"));
 		}
-		if (Input.GetAxis ("Pause") >= 1f)
-		{
-			//levelSettings.SendMessage("PauseGame");
-			Debug.Log ("Pause Game");
-			pauseDown = true;
-		}
-		if (Input.GetAxis ("Pause") <= .5f && pauseDown == true) 
-		{
-			levelSettings.SendMessage("PauseGame");
-			pauseDown = false;
-		}
-		//Debug.Log ("Pause Axis: " + Input.GetAxis ("Pause"));
-		Debug.Log (pauseDown);
 	}
 }
