b8b93d8e341dc665ac8589a041558d9a67fd4aa1
Nick Mudry <Nick@Nicholass-MacBook-Air.local>
2014-08-27 20:00:44 -0400

    First Test of Social Sharing!

--- Assets/Scripts/SocialSharing.cs
+++ Assets/Scripts/SocialSharing.cs
@@ -17,10 +17,12 @@ public class SocialSharing : MonoBehaviour {
 	void OnMouseDown () {
 		if (gameObject.name == "TwitterButton")
 		{
+			Debug.Log ("Twitter Share");
 			socialManager.TwitterPost("I just scored " + roundScore + " in Lost In Space on iOS! Check it out: http://bit.ly/LostInSpace #ios #mobilegames");
 		}
 		if (gameObject.name == "FacebookButton")
 		{
+			Debug.Log ("Facebook Share");
 			socialManager.FacebookPost("I just scored " + roundScore + " in Lost In Space on iOS! Check it out: http://bit.ly/LostInSpace #ios #mobilegames");
 		}
 	}
